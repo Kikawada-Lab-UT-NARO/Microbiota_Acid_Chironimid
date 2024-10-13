c33b78771489d742f78e30a88d671cb73fd59ac9
NakanishiEitaUT <70379670+NakanishiEitaUT@users.noreply.github.com>
2024-10-13 19:53:28 +0900

    Update lefse_analysis.sh

--- Full-length_16S_rRNA_analyses/lefse_analysis.sh
+++ Full-length_16S_rRNA_analyses/lefse_analysis.sh
@@ -1,11 +1,10 @@
 
 # Order level
-lefse_format_input.py lefse_nanopore_order.txt lefse_nanopore_order.in -c 1 -s 2 -u 3 -o 1000000
-lefse_run.py lefse_nanopore_order.in lefse_nanopore_order.res -y 1
-lefse_plot_res.py lefse_nanopore_order.res lefse_nanopore_order.svg --format svg --autoscale 0
-
+lefse_format_input.py feature-table_lefse_col_wild_vs_rearing_order_nanopore.txt feature-table_lefse_col_wild_vs_rearing_order_nanopore.in -c 1 -s 2 -u 3 -o 1000000
+lefse_run.py feature-table_lefse_col_wild_vs_rearing_order_nanopore.in feature-table_lefse_col_wild_vs_rearing_order_nanopore.res -y 1
+lefse_plot_res.py feature-table_lefse_col_wild_vs_rearing_order_nanopore.res feature-table_lefse_col_wild_vs_rearing_order_nanopore.svg --format svg --autoscale 0
 
 # Species level
-lefse_format_input.py lefse_nanopore_genus.txt lefse_nanopore_genus.in -c 1 -s 2 -u 3 -o 1000000
-lefse_run.py lefse_nanopore_order.in lefse_nanopore_genus.res -y 1
-lefse_plot_res.py lefse_nanopore_genus.res lefse_nanopore_genus.svg --format svg --autoscale 0
+lefse_format_input.py feature-table_lefse_col_wild_vs_rearing_genus_nanopore.txt feature-table_lefse_col_wild_vs_rearing_genus_nanopore.in -c 1 -s 2 -u 3 -o 1000000
+lefse_run.py feature-table_lefse_col_wild_vs_rearing_genus_nanopore.in feature-table_lefse_col_wild_vs_rearing_genus_nanopore.res -y 1
+lefse_plot_res.py feature-table_lefse_col_wild_vs_rearing_genus_nanopore.res feature-table_lefse_col_wild_vs_rearing_genus_nanopore.svg --format svg --autoscale 0
